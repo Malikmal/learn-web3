0acd99a8eee7f94460c94944aa32f18194a6432c
ikmalil birri <malikmal98@gmail.com>
2022-08-06 18:22:04 +0700

    fixing : error typescript calling env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider
-      appId={process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID}
-      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
+      appId={`${process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID}`}
+      serverUrl={`${process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}`}
     >
       <Component {...pageProps} />
     </MoralisProvider>
